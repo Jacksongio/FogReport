f8e8293f7271e34adc9efe47695e0d6cc4c6ea6a
Jackson Giordano <jackson@giordano.us>
2026-05-07 09:36:00 -0400

    revert

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
 
-export default convexAuthNextjsMiddleware({ verbose: true });
+export default convexAuthNextjsMiddleware();
 
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
